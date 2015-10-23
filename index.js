8540596cd9400ea039264cbce60bb4e9a2587d14
Larlyn <strawww@gmail.com>
2015-10-23 16:18:41 +1100

    Implement validate, use correct url for .search

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-// * signin - email and password, returns matching entity or invalid
-// * token validation - pass JWT, returns matching entity or invalid
-// * accept entity queries - run query against auth API and return results
-// * create entities
-// * update entities (adding / deleting permissions, changing name, adding / deleting emails)
-
-// auth.signIn
-// auth.validate
-// auth.read
-// auth.create
-// auth.update
-
 var request = require('request');
-var AUTH_URL = process.env.AUTH_URL || 'http://localhost:9000';
+var AUTH_URL = process.env.AUTH_URL || 'http://auth-ordermentum-sandpit.prismatik.com.au';
 
-exports.validate = function(jwt) {
-
+exports.validate = function(token) {
+  return new Promise(function(resolve, reject){
+    request.get('/entities?token=' + token, function(err, res, body){
+      if (err) reject(err);
+      return resolve(body);
+    });
+  });
 };
 
 exports.signIn = function(email, password) {
   return new Promise(function(resolve, reject){
     request.post(AUTH_URL + '/login', { email: email, password: password }, function(err, res, body){
       if (err) reject(err);
-
       return resolve(body);
     })
   });
@@ -31,19 +23,26 @@ exports.create = function(data) {
   return new Promise(function(resolve, reject){
     request.post(AUTH_URL + '/entities', data, function(err, res, body){
       if (err) reject(err);
+      return resolve(body);
+    });
+  });
+};
 
+exports.search = function(type, uuid) {
+  return new Promise(function(resolve, reject){
+    request.get(AUTH_URL + '/entities?perm.type=' + type + '&perm.entity=' + uuid, function(err, res, body){
+      if (err) reject(err);
       return resolve(body);
     })
   });
 };
 
-exports.read = function(query) {
+exports.getAll = function() {
   return new Promise(function(resolve, reject){
     request.get(AUTH_URL + '/entities', function(err, res, body){
       if (err) reject(err);
-
       return resolve(body);
-    })
+    });
   });
 };
 
@@ -51,7 +50,6 @@ exports.update = function(id, data) {
   return new Promise(function(resolve, reject){
     request.put(AUTH_URL + '/entities/' + id, data, function(err, res, body){
       if (err) reject(err);
-
       return resolve(body);
     });
   });
