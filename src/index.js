62d0880cbd0b7077fb3fc86d8572b5ebbf07d1c0
Simon Taylor <nizmox@gmail.com>
2016-05-16 17:04:13 +1000

    set content-type for requests

--- src/index.js
+++ src/index.js
@@ -73,7 +73,8 @@ function configureInterceptors(instance, url, { username, password } = {}) {
   instance.interceptors.request.use(config => {
     config.url = `${url}${config.url}`;
     config.headers = {
-      Authorization: `Basic ${auth}`
+      'Authorization': `Basic ${auth}`,
+      'Content-Type': 'application/json'
     }
 
     return config;
