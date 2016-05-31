769172da52ba22125cdb388d1e699ad00ab16c29
Nathan Winch <nwinch@gmail.com>
2016-05-31 10:20:59 +1000

    Compile

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
+
+var _standardHttpError = require('standard-http-error');
+
+var _standardHttpError2 = _interopRequireDefault(_standardHttpError);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -121,7 +127,16 @@ function configureInterceptors(instance, url) {
 
   instance.interceptors.response.use(function (res) {
     return res.data;
+  }, function (err) {
+    if (!(err instanceof Error) && (typeof err === 'undefined' ? 'undefined' : _typeof(err)) === "object") {
+      return Promise.reject(errorify(err));
+    }
+    return Promise.reject(err);
   });
 
   return instance;
+}
+
+function errorify(res) {
+  return new _standardHttpError2.default(res.status, res.statusText, { response: res });
 }
